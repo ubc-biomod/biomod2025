e912cd5a5ebc48e111c2cd9ea62aaa10559959b5
CarsonKiibi <carrotscarson@gmail.com>
2025-03-02 18:12:18 -0800

    -examples added and new root (index.astro)

---
+++ src/react/examplebetterdata.tsx
@@ -0,0 +1,15 @@
+import dogImage from "../assets/images/dogs.jpg";
+import catImage from "../assets/images/cats.jpg";
+
+export const exampleData = [
+    {
+        imageSrc: dogImage.src,
+        text: "This is a dog!",
+        imageClass: "w-1/3"
+    },
+    {
+        imageSrc: catImage.src,
+        text: "This is a cat.",
+        imageClass: "w-1/2"
+    }
+]
