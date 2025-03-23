0979a8b581380ecdfacaa2716438a05e896c137a
JosephDeng317 <75818199+JosephDeng317@users.noreply.github.com>
2025-03-23 16:03:38 -0700

    initial navbar commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,61 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	  },
-    integrations: [starlight({
-        title: 'My Docs',
-        social: {
-            github: 'https://github.com/withastro/starlight',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    starlight({
+      title: "UBC BIOMOD",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      logo: {
+        src: "./src/assets/logo.png",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
         },
-        sidebar: [
-            {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
-        ],
-		}), react()],
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "Home",
+          link: "/",
+          badge: "Hidden",
+        },
+        {
+          label: "Ideas",
+          autogenerate: { directory: "ideas" },
+        },
+        {
+          label: "ELSI",
+          autogenerate: { directory: "elsi" },
+        },
+        {
+          label: "Lab Notebook",
+          autogenerate: { directory: "lab-notebook" },
+          badge: {
+            text: "Call to Action",
+          },
+        },
+      ],
+      components: {
+        Header: "./src/components/overrides/Header.astro",
+      },
+    }),
+    react(),
+  ],
 });
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { BsBoxArrowUpRight } from 'react-icons/bs';
+import { FiMenu, FiX } from 'react-icons/fi';
+
+export default function Navbar({ links, currentBase, callToActionLink, isNormalPage }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col lg:flex-row gap-x-16 grow justify-end lg:justify-center items-end lg:items-center basis-1/2">
+      <p>
+        Normal page is: {isNormalPage? "true" : "false"}
+      </p>
+      {/* Hamburger Icon (Mobile) */}
+      <div className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <FiX className="text-2xl" /> : <FiMenu className="text-2xl" />}
+      </div>
+
+      {/* Navigation Links */}
+      <div className={`flex flex-col lg:flex-row gap-x-8 items-end lg:items-center lg:gap-x-16 ${isOpen ? 'block' : 'hidden'} lg:flex`}>
+        {links.map((link) => (
+          <a
+            key={link.href}
+            href={link.href}
+            className="group dark:text-white text-navText font-semibold hover:opacity-70 transition duration-300"
+          >
+            {link.title}
+            <div
+              className={`max-w-0 group-hover:left-0 group-hover:max-w-full h-[0.2rem] bg-black transition-all duration-300 origin-center relative 
+                ${link.base === currentBase ? "left-0 max-w-full" : "left-1/2"}`}
+            />
+          </a>
+        ))}
+        {isNormalPage ? (
+          <a href={callToActionLink.href} className="block lg:hidden group text-navText font-semibold hover:opacity-70 transition duration-300">
+            <span>{callToActionLink.title}</span>
+          </a>
+        ) : (
+          <div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
