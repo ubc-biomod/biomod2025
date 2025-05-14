12e9d45a971b3ad8ae4f067ccbf099f7cf27002e
CarsonKiibi <carrotscarson@gmail.com>
2025-05-14 16:38:21 -0700

    -updated astro to use footer and changed some styles

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,31 @@
+// Footer.tsx
+import logo from "../assets/images/logo.png";
+
+type FooterLink = {
+  title: string;
+  href: string;
+};
+
+export default function Footer({ links }: { links: FooterLink[] }) {
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-between px-8 py-6 bg-white shadow-t-md gap-y-4">
+      <div className="flex items-center space-x-2">
+        <img src={logo.src} alt="UBC BIOMOD Logo" className="h-10 w-10" />
+        <span className="font-bold text-lg">UBC BIOMOD</span>
+      </div>
+
+      <ul className="flex flex-wrap justify-center space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <a
+              href={link.href}
+              className="text-gray-800 hover:text-blue-600 transition"
+            >
+              {link.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
