e4a0e1efaa94aa5ac2b9be366805dca952bed2cf
Joseph Deng <josephdeng317@gmail.com>
2025-05-25 20:04:20 -0700

    added logo to navbar and minor restyle

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,5 +1,4 @@
 // Footer.tsx
-import logo from "../assets/images/logo.png";
 
 type FooterLink = {
   title: string;
@@ -8,11 +7,7 @@ type FooterLink = {
 
 export default function Footer({ links }: { links: FooterLink[] }) {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between px-8 py-6 bg-white shadow-t-md gap-y-4">
-      <div className="flex items-center space-x-2">
-        <img src={logo} alt="UBC BIOMOD Logo" className="h-10 w-10" />
-        <span className="font-bold text-lg">UBC BIOMOD</span>
-      </div>
+    <div className="flex flex-col md:flex-row justify-center px-8 py-6 bg-white shadow-t-md gap-y-4">
 
       <ul className="flex flex-wrap justify-center space-x-6">
         {links.map((link) => (
