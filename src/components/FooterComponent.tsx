44a93168fa0f87a3829ab2d05eae89a9a3eca0b0
CarsonKiibi <carrotscarson@gmail.com>
2025-05-29 22:32:34 -0700

    -reverted footer

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,5 +1,7 @@
 // Footer.tsx
 
+import logo from "../assets/images/logo.png";
+
 type FooterLink = {
   title: string;
   href: string;
@@ -7,7 +9,11 @@ type FooterLink = {
 
 export default function Footer({ links }: { links: FooterLink[] }) {
   return (
-    <div className="flex flex-col md:flex-row justify-center px-8 py-6 bg-navbar shadow-t-md gap-y-4">
+    <div className="flex flex-col md:flex-row items-center justify-between px-8 py-6 bg-white shadow-t-md gap-y-4">
+      <div className="flex items-center space-x-2">
+        <img src={logo} alt="UBC BIOMOD Logo" className="h-10 w-10" />
+        <span className="font-bold text-lg">UBC BIOMOD</span>
+      </div>
 
       <ul className="flex flex-wrap justify-center space-x-6">
         {links.map((link) => (
