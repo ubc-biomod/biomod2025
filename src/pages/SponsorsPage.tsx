9a6cd2f1bc7a6de0ad3035166c78380fbdaa89ed
CarsonKiibi <carrotscarson@gmail.com>
2025-05-29 23:41:07 -0700

    Revert "Merge branch 'joseph' into carson/mainpage-demo"
    This reverts commit 97f9870df39931c0959824e94e4f5b30ed8865ed, reversing
    changes made to 44a93168fa0f87a3829ab2d05eae89a9a3eca0b0.

--- src/pages/SponsorsPage.tsx
+++ src/pages/SponsorsPage.tsx
@@ -1,10 +1,7 @@
-import SponsorCards from '../components/SponsorCards'
-
 function SponsorsPage() {
     return (
         <div>
             Hello world
-            <SponsorCards></SponsorCards>
         </div>
     )
 }
