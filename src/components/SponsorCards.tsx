010c17ad6444aad4cc2c7971f33150a0a1b33f54
JosephDeng317 <75818199+JosephDeng317@users.noreply.github.com>
2025-10-08 20:26:43 -0700

    Basic sponsors

--- src/components/SponsorCards.tsx
+++ src/components/SponsorCards.tsx
@@ -5,19 +5,16 @@ import { useEffect, useRef } from "react"
 export default function SponsorCards() {
     return (
         <div>
-            <StickyCard title="Platinum" i={0} cardClass="bg-indigo-100 min-h-100 text-center">
+            <StickyCard title="Gold" i={0} cardClass="bg-yellow-400 min-h-100 text-center">
                 <p>
-                    Platinum sponsors are the best of the best!
-                </p>
-            </StickyCard>
-            <StickyCard title="Gold" i={1} cardClass="bg-yellow-400 min-h-100 text-center">
-                <p>
-                    Gold sponsors are the second best!
+                    Gold sponsors!
+                    UBC School of Biomedical Engineering, UBC Engineering
                 </p>  
             </StickyCard>
-            <StickyCard title="Bronze" i={2} cardClass="bg-yellow-600 min-h-100 text-center">
+            <StickyCard title="Bronze" i={1} cardClass="bg-yellow-600 min-h-100 text-center">
                 <p>
-                    Bronze sponsors are the third best!
+                    Bronze sponsors!
+                    UBC Life Science Institute, Sino Biological, and New England Biolabs
                 </p>  
             </StickyCard>
         </div>
